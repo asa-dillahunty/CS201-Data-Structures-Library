38701b8cc1df235aa198253790671e55db77ebbf
Asa Dillahunty <stantondillahunty@gmail.com>
2019-09-12 00:36:30 -0500

    Added comments to the code

--- cda.cpp
+++ cda.cpp
@@ -17,24 +17,11 @@ class CDA {
 		int cap;
 		int head;
 		T *list; 
-		
-		/*
-		 * If I get rid of the need for the if statement
-		 * I could save time
-		 * 
-		 * To do so just check when plausible for index to 
-		 * be out of bounds
+	
+		/**
+		 * Increases the size of the internal array
+		 * and copies all values over.
 		 */
-		int relativeIndex(int i) {
-			
-			if (i<0 || i>this->size-1) {
-				throw;
-			}
-			else {
-				return (this->head+i)%this->cap;
-			}
-		}
-
 		void upsize() {
 			
 			int newCap = this->cap*2;			
@@ -51,6 +38,10 @@ class CDA {
 			this->head=0;
 		}
 
+		/**
+		 * Decreases the size of the internal array
+		 * and copies all values over.
+		 */
 		void downsize() {
 			if (size==0) return;
 			
@@ -68,8 +59,36 @@ class CDA {
 			this->head=0;
 		}
 
+		/**
+		 * Returns the real index when given the index relative to the head
+		 * (Starting with the head of the array as index zero
+		 *  and iterating from there)
+		 *
+		 *  @param index: the index relative to the head of the array
+		 *  @return: returns the actual index for the internal array
+		 */
+		/*
+		 * If I get rid of the need for the if statement
+		 * I could save time
+		 * 
+		 * To do so just check when plausible for index to 
+		 * be out of bounds, this should just be when the
+		 * operator calls it
+		 */
+		int relativeIndex(int index) {
+			
+			if (index<0 || index>this->size-1) {
+				throw;
+			}
+			else {
+				return (this->head+index)%this->cap;
+			}
+		}
 		
 	public:
+		/**
+		 * This constructs the CDA with an initial capacity of 2
+		 */
 		CDA() {
 			this->size=0;
 			this->cap=2;
@@ -77,6 +96,11 @@ class CDA {
 			this->list=new T[cap];
 		}
 
+		/**
+		 * this constructs the CDA with an initial capacity of s
+		 *
+		 * @param s: the given initial capacity
+		 */
 		CDA(int s) {
 			this->size=0;
 			this->cap=s;
@@ -84,15 +108,28 @@ class CDA {
 			this->list=new T[cap];
 		}
 
+		/**
+		 * Deconstructs the CDA
+		 */
 		~CDA() {
 			delete [] this->list;
 		}
 
-		T& operator[](int i) {
-			return this->list[this->relativeIndex(i)];
+		/**
+		 * Defines the bracket operator so it can be used on the CDA class
+		 *
+		 * @param index: the index relative to the head of the array
+		 * @return: returns the data stored at the index
+		 */
+		T& operator[](int index) {
+			return this->list[this->relativeIndex(index)];
 		}
 
-
+		/**
+		 * Adds data at the end of the circular array, relative to the head
+		 *
+		 * @param data: the data to be added to the end of the array
+		 */
 		void addEnd(T data) {
 			
 			if (this->size==this->cap) {
@@ -105,6 +142,11 @@ class CDA {
 			}
 		}
 
+		/**
+		 * Adds data at the front of the circular array, relative to the head
+		 *
+		 * @param data: the data to be added to the front of the array
+		 */
 		void addFront(T data) {
 			
 			if (this->size==this->cap) {
@@ -112,12 +154,15 @@ class CDA {
 				this->addFront(data);
 			}
 			else {
-				this->head = (this->head+this->cap-1)%this->cap;
+				this->head = (this->head+this->cap-1)%this->cap; //capacity is added here to avoid the mod of negative numbers when head = 0
 				this->list[this->head]=data;
 				this->size++;
 			}
 		}
 
+		/**
+		 * Removes the last element in the CDA relative to the head
+		 */
 		/* Should I free the deleted Element??? */
 		void delEnd() {
 			
@@ -129,6 +174,9 @@ class CDA {
 			}
 		}
 
+		/**
+		 * Removes the first element in the CDA relative to the head
+		 */
 		void delFront() {
 
 			T data = this->list[this->head];
@@ -140,14 +188,27 @@ class CDA {
 			}
 		}
 		
+		/**
+		 * Returns the number of elements in the CDA
+		 *
+		 * @return: returns the size of the CDA
+		 */
 		int length() {
 			return this->size;
 		}
 
+		/**
+		 * Returns the capacity of the CDA
+		 *
+		 * @return: returns the capacity of the CDA
+		 */
 		int capacity() {
 			return this->cap;
 		}
 
+		/**
+		 * Prints the CDA starting from the head
+		 */
 		void printList() {
 			for (int i=0;i<size;i++) {
 				std::cout << this->list[this->relativeIndex(i)] << std::endl;
